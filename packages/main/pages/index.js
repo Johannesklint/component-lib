018dd99bead35f77221e0e81d4fbbbf5ae9f100b
johkli <johannes.klint@etraveligroup.com>
2020-07-24 14:12:19 +0200

    Add Checkbbox to main app

--- packages/main/pages/index.js
+++ packages/main/pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import Input from '@library/input'
+import { CheckboxGroup, Checkbox } from '@library/checkbox'
 import { isFunction } from '@library/utils'
-import { useReducer } from 'react'
 
 function Button({ type, onClick, children, className }) {
   function handleClick() {
@@ -14,90 +14,6 @@ function Button({ type, onClick, children, className }) {
     <button type={type} onClick={handleClick} className={className}>
       {children}
     </button>
-  )
-}
-
-function Checkbox({ id, name, value, checked, onChange, children }) {
-  return (
-    <>
-      <label htmlFor={id}>
-        <input
-          type="checkbox"
-          id={id}
-          name={name}
-          value={value}
-          checked={checked}
-          onChange={onChange}
-        />
-        {children}
-      </label>
-    </>
-  )
-}
-
-function CheckboxGroup({ legend, children }) {
-  const initState = {
-    ...React.Children.map(children, (child) => {
-      return {
-        [child.props.name]: {
-          id: child.props.id,
-          value: child.props.value,
-          checked: child.props.checked || false,
-        },
-      }
-    }).reduce(
-      (acc, item) => ({
-        ...acc,
-        ...item,
-      }),
-      {}
-    ),
-  }
-
-  function fixState(oldState, payload) {
-    return Object.entries(oldState).reduce((acc, [key, value]) => ({
-      ...acc,
-      [key]: {
-        ...value,
-        checked: false,
-      },
-    }), {})
-  }
-
-  function reducer(state, action) {
-    const { payload, type } = action
-    switch (type) {
-      case 'CHANGE':
-        return {
-          ...fixState(state, payload),
-          [payload.name]: {
-            ...state[payload.name],
-            checked: payload.checked,
-          },
-        }
-      default:
-        throw new Error()
-    }
-  }
-  const [state, dispatch] = useReducer(reducer, initState)
-  return (
-    <fieldset>
-      {legend}
-      {React.Children.map(children, (child) =>
-        React.cloneElement(child, {
-          ...state[child.props.name],
-          onChange: (event) => {
-            dispatch({
-              type: 'CHANGE',
-              payload: {
-                name: event.target.name,
-                checked: event.target.checked,
-              },
-            })
-          },
-        })
-      )}
-    </fieldset>
   )
 }
 
