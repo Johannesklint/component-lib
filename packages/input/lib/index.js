dcb45581447a765118be28d6627c4825b4239f1f
johkli <johannes.klint@etraveligroup.com>
2020-07-31 12:44:34 +0200

    Add label corret to input

--- packages/input/lib/index.js
+++ packages/input/lib/index.js
@@ -1,7 +1,7 @@
 const React = require('react')
 const { isFunction } = require('@library/utils')
 
-function Input({ handleOnChange, ...props }) {
+function Input({ handleOnChange, id, label, ...props }) {
   const [value, setValue] = React.useState('')
 
   function onChange(event) {
@@ -11,7 +11,12 @@ function Input({ handleOnChange, ...props }) {
     }
   }
 
-  return <input {...props} value={value} onChange={onChange} />
+  return (
+    <label htmlFor={id}>
+      {label}
+      <input {...props} id={id} value={value} onChange={onChange} />
+    </label>
+  )
 }
 
 module.exports = Input
